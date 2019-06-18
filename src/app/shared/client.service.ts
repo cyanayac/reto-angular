63f91134311a501cdb4c0a90f28e25428cfb975c
cyanayaco <cyanayaco@gmail.com>
2019-06-18 11:05:42 -0500

    add clientService

---
+++ src/app/shared/client.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private firebase: AngularFireDatabase) { }
+  clientList: AngularFireList<any>;
+
+
+  form = new FormGroup({
+    $key: new FormControl(null),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    age: new FormControl(''),
+    birthdate: new FormControl('')
+  });
+
+  getClients(){
+    this.clientList = this.firebase.list('clients');
+    return this.clientList.snapshotChanges();
+  }
+
+  insertClient(client){
+    this.clientList.push({
+      name: client.name,
+      lastName: client.lastName,
+      age: client.age,
+      birthdate: client.birthdate
+    });
+  }
+}
