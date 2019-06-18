c01e634c7026cb396767dc28d4250d0ef93bdafa
cyanayaco <cyanayaco@gmail.com>
2019-06-18 11:08:08 -0500

    add clientComponent

---
+++ src/app/client/client.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../shared/client.service';
+
+@Component({
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.css']
+})
+export class ClientComponent implements OnInit {
+
+  constructor( private clientService: ClientService) { }
+  submitted: boolean;
+  showSuccessMessage: boolean;
+  formControls = this.clientService.form.controls;
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if(this.clientService.form.valid){
+      if (this.clientService.form.get('$key').value == null)
+        this.clientService.insertClient(this.clientService.form.value);
+        this.showSuccessMessage = true;
+        setTimeout(() => this.showSuccessMessage = false, 3000);
+      this.submitted = false;
+    }
+    
+    // else{
+    //   this.clientService.updateClient(this.clientService.form.value);
+    // }
+
+  }
+
+}
