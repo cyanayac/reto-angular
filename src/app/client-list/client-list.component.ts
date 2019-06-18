691c42d4193830209e216f49f0e32d4d45f5655f
cyanayaco <cyanayaco@gmail.com>
2019-06-18 14:39:20 -0500

    add fechaDeMuerte

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -10,14 +10,16 @@ export class ClientListComponent implements OnInit {
 
   constructor( private clientService: ClientService) { }
   clientArray = [];
-  promAge= ""
+  promAge= "";
 
   ngOnInit() {
     this.clientService.getClients().subscribe(
+      
       list => {
         this.clientArray = list.map(item =>{
           return {
             $key: item.key,
+            deathDate: this.add_years(parseFloat(item.payload.val().age)),
             ...item.payload.val()
           };
         });
@@ -26,6 +28,7 @@ export class ClientListComponent implements OnInit {
       
   }
 
+  // Canculando edad promedio
   calculateProm(){
     let numClients = this.clientArray.length;
     let sumAge = 0;
@@ -37,7 +40,14 @@ export class ClientListComponent implements OnInit {
       this.promAge = (sumAge/ numClients).toFixed(2);
       console.log(this.promAge); 
     }
+  }
 
+  // Obteniendo fecha aleatoria con la edad
+  add_years(n) {
+    let dt = new Date();
+    let mth = new Date(dt.setFullYear(dt.getFullYear() + n));      
+    let dy = new Date(mth.setMonth(mth.getMonth() + n)); 
+    return new Date(dy.setDate(dy.getDate() + n)); 
   }
 
 }
