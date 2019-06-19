c4443ca0a8ec3081ba763510aa28cb5c6cd94dc9
cyanayaco <cyanayaco@gmail.com>
2019-06-18 20:05:05 -0500

    change: environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBgaQrapV2ZxgnA4_1ljgLM1xXYVEb4veY",
+    authDomain: "test-challenge3246.firebaseapp.com",
+    databaseURL: "https://test-challenge3246.firebaseio.com",
+    projectId: "test-challenge3246",
+    storageBucket: "test-challenge3246.appspot.com",
+    messagingSenderId: "940069454292",
+    appId: "1:940069454292:web:55de88d6bf81ad9e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: "AIzaSyBgaQrapV2ZxgnA4_1ljgLM1xXYVEb4veY",
     authDomain: "test-challenge3246.firebaseapp.com",
     databaseURL: "https://test-challenge3246.firebaseio.com",
     projectId: "test-challenge3246",
-    storageBucket: "",
+    storageBucket: "test-challenge3246.appspot.com",
     messagingSenderId: "940069454292",
     appId: "1:940069454292:web:55de88d6bf81ad9e"
   }
